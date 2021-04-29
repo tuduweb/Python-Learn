ff2a6952eecd3d5b6bb3933716174f752f2c4322
bin <tuduweb@qq.com>
2021-04-29 15:58:25 +0800

    更新最简单的提取信息。

--- spider-HomeworkReminder/educoder/educoder/spiders/homework.py
+++ spider-HomeworkReminder/educoder/educoder/spiders/homework.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import json
 
 class HomeworkSpider(scrapy.Spider):
     name = 'homework'
@@ -14,5 +14,26 @@ class HomeworkSpider(scrapy.Spider):
         yield scrapy.Request(self.start_urls[0], cookies=cookies)
 
     def parse(self, response):
-        print(response.text)
+        # print(response.text)
+        rs = json.loads(response.text)
+        homeworks = rs.get('homeworks')
+
+
+        # {'homework_id': 243356, 'name': 'C语言基本输入输出实训', 'private_icon': True, 'status': ['已截止'], 'status_time': '评阅剩余时间：63 天 8 小时 18 分 ', 'time_status': 5, 'allow_late': False, 'author': '李志清', 'author_img': 'avatars/User/21310?t=1612439758', 'author_login': 'lizhiqing', 'created_at': '2021-03-01', 'upper_category_name': '第1章 C语言概述', 'position': 27, 'publish_immediately': False, 'end_immediately': False, 'commit_count': 96, 'uncommit_count': 0, 'all_count': 96, 'compelete_count': 96, 'shixun_identifier': '3aexl5my', 'shixun_status': 2, 'shixun_name': 'C语言基本输入输出实训', 'opening_time': None, 'is_enter_shixun': True, 'shixun_enter_status': 0}
+
+        for homework in homeworks:
+            # print(homework)
+            all_count = homework.get('all_count')
+            uncommit_count = homework.get('uncommit_count')
+            commit_count = homework.get('commit_count')
+            compelete_count = homework.get('compelete_count')
+            print('#'*40)
+            print(homework.get('name'))
+            print(homework.get('status'))
+            print(homework.get('status_time'))
+            print("未开始:{0} 尝试中:{1} 已完成:{2}".format(uncommit_count, commit_count - compelete_count, compelete_count))
+        
+        # request: {"page":3,"limit":20,"order":"work_score","b_order":"desc","coursesId":"11157","categoryId":"275431"}
+        # https://data.educoder.net/api/homework_commons/275431/works_list.json
+        
         pass
